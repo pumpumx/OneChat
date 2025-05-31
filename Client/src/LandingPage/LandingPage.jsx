c2e150e75f6c49942d1ffb3747485bcf6687dac7
pumpumx <trishuchibhot14@gmail.com>
2025-06-01 02:49:40 +0530

    Done with landing page

--- Client/src/LandingPage/LandingPage.jsx
+++ Client/src/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import VideoSection from './components/VideoSection'
 function LandingPage() {
   return (
     <>
-      <div className='w-full scrollbar-hide ' >
+      <div className='w-full ' >
       <video muted autoPlay={true} loop playsInline className='fixed top-0 left-0 -z-1'>
             <source src='/starBg.mp4' type='video/mp4' />            
           </video>
@@ -17,10 +17,13 @@ function LandingPage() {
           <div className='w-full h-[100vh] z-10'>
             <CenterScreen />
           </div>
-          <div className='w-full sticky top-0 h-[200vh]  z-0 '>
+        </div>
+        <div className='w-full  h-[200vh]  z-0 '>
             <VideoSection />
           </div>
-        </div>
+          <div className='w-full h-[100vh]'>
+
+          </div>
       </div>
 
     </>
